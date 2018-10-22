d92bc27506fdd750dea337290e3bc31e8c089f5e
hieu ta <42407085+hieuxuanta@users.noreply.github.com>
2018-10-22 16:35:22 +0700

    Update login.php

--- login.php
+++ login.php
@@ -7,14 +7,17 @@
     <style>
     body{
         background-color:lightblue;
-        padding-top:400px;
-        padding-left:850px;
+        padding-top:290px;
+        padding-left:580px;
     }
     </style>
 </head>
 
 <body>
 <?php
+	session_start();
+	require_once('connectt.php');
+	echo '<br/>';
     if(isset($_POST["name"]))
     {
         if(isset($_POST["password"]))
@@ -32,16 +35,30 @@
                 echo "You must fill password <br>";
                 $ck_dang_nhap = false;
             }
-            if(strlen ($Username) < 6)
+            if(strlen ($Username) < 4)
             {
                 echo "Your Username must at least 6 characters <br>";
                 $ck_dang_nhap = false;
             }
             if($ck_dang_nhap)
             {
-                echo "Sign in successfully<br>";
+                //echo "Sign in successfully<br>";
+                $password = md5($password);
+                $sql = "select * from user where username=
+                '$Username' and password='$password'";
+                echo $sql;
+                $query = mysqli_query($conn,$sql);
+                $num_rows = mysqli_num_rows($query);
+                if($num_rows == 0)
+                {
+                    echo 'Username khong ton tai';
+                }else
+                    {
+                        $_SESSION['Username'] = $Username;
+                        header('Location: user_management.php');
+                    }
                 
-                echo '<a href = "https://www.facebook.com/gtechvntraining/" >This is our class !</a>';
+                //echo '<a href = "https://www.facebook.com/gtechvntraining/" >This is our class !</a>';
             }
             else 
             {
